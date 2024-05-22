0531e234b0f959f892b47061b26389da3b916e3b
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-21 21:57:13 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function mus1() {
+    var reprod = document.getElementById('reprod')
+    var título = document.querySelector('h1')
+    reprod.style.display = 'Block'
+    título.style.display = 'none' 
+    reprod.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZPdk5GaIDjo?si=PMHOCYKiZpmum7Hy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'       
+}
+
+function mus2() {
+    var reprod = document.getElementById('reprod')
+    var título = document.querySelector('h1')
+    reprod.style.display = 'Block'
+    título.style.display = 'none' 
+    reprod.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/RvDt_KtOzbc?si=PsN-QjB-Xk3MO9UD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> '   
+}
+
+function mus3() {
+    var reprod = document.getElementById('reprod')
+    var título = document.querySelector('h1')
+    reprod.style.display = 'Block'
+    título.style.display = 'none' 
+    reprod.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/UIdwCrfDNjU?si=0ZdEfLCITjRLnv8G" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+}
+
+function mus4() {
+    var reprod = document.getElementById('reprod')
+    var título = document.querySelector('h1')
+    reprod.style.display = 'Block'
+    título.style.display = 'none' 
+    reprod.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/3LiztfE1X7E?si=rGfkhADfe7e9YnG1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+}
